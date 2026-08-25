c59286fc34d23175be0eabde3267684b75851ba9
Sapphira Riza <safthelamb@gmail.com>
2026-08-24 22:38:12 -0700

    corner icon functions

--- lib/icon.lua
+++ lib/icon.lua
@@ -52,12 +52,18 @@ function fds_icon.alignment.is_bottom(alignment)
 	return alignment == fds_icon.alignment.bottom_left or alignment == fds_icon.alignment.bottom_center or alignment == fds_icon.alignment.bottom_right
 end
 
----Moves and scales the existing icon
----@param icon data.IconData
+---Moves and scales an icon
+---@param icon data.IconData|data.IconData[]
 ---@param in_scale double
 ---@param in_shift data.Vector
----@return data.IconData
+---@return data.IconData|data.IconData[]
 function fds_icon.adjust_icon(icon, in_scale, in_shift)
+	if #icon > 0 then
+		for _,subicon in pairs(icon) do
+			fds_icon.adjust_icon(subicon, in_scale, in_shift)
+		end
+		return icon
+	end
 	local shift = icon.shift and {icon.shift.x or icon.shift[1], icon.shift.y or icon.shift[1]} or {0,0}
 	shift = math2d.position.add(math2d.position.multiply_scalar(shift, in_scale), in_shift)
 	if icon.shift and icon.shift.x then
@@ -69,13 +75,13 @@ function fds_icon.adjust_icon(icon, in_scale, in_shift)
 	return icon
 end
 
----Constructs an icon for the given prototype, if it exists.
----@param prototype_type data.PrototypeBaseName Type of the prototype to get the icon from
----@param prototype_name string Name of the prototype to get the icon from
----@param alignment fds_icon.alignment
+---Constructs an icon for the given prototype, if it exists. Supports prototypes with multiple icons.
+---@param prototype_type string Type of the prototype to get the icon from, or nil to use a file directly.
+---@param prototype_name string Name of the prototype to get the icon from, or the mod-relative filepath.
+---@param alignment fds_icon.alignment Which "corner" to put the sub-icon in
 ---@return data.IconData,data.IconData?,data.IconData?,data.IconData?,data.IconData?,data.IconData?,data.IconData?,data.IconData?,data.IconData?,data.IconData?
 function fds_icon.make_corner_icon(prototype_type, prototype_name, alignment)
-	local prototype = data.raw[prototype_type][prototype_name]
+	local prototype = prototype_type and data.raw[prototype_type][prototype_name] or prototype_name
 	if prototype then
 		local shift = {0,0}
 
@@ -90,16 +96,21 @@ function fds_icon.make_corner_icon(prototype_type, prototype_name, alignment)
 			shift[2] = 8
 		end
 
-		if prototype.icons then
-			local icons = util.table.deepcopy(prototype.icons)
-			for _,icon in pairs(icons) do
-				fds_icon.adjust_icon(icon, 0.5, shift)
-				icon.draw_background = true
+		if type(prototype) == "string" then
+			return fds_icon.adjust_icon({icon=prototype, draw_background=true}, 0.5, shift)
+		else
+			if prototype.icons then
+				local icons = util.table.deepcopy(prototype.icons)
+				assert(icons)
+				fds_icon.adjust_icon(icons, 0.5, shift)
+				for _,icon in pairs(icons) do
+					icon.draw_background = true
+				end
+				return table.unpack(icons)
+			elseif prototype.icon then
+				assert(type(prototype.icon) == "string" and helpers.is_valid_sprite_path(prototype.icon))
+				return fds_icon.adjust_icon({icon=prototype.icon, icon_size=prototype.icon_size, draw_background=true}, 0.5, shift)
 			end
-			return table.unpack(icons)
-		elseif prototype.icon then
-			local icon = fds_icon.adjust_icon({icon=prototype.icon, icon_size=prototype.icon_size, draw_background=true}, 0.5, shift)
-			return icon
 		end
 	end
 	return {icon="__core__/graphics/empty.png"}
